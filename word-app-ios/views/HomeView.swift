960c3b3b049cc36a31e3d9c58808fdc26c12c59d
Yijia Chen <kevinc110604@gmail.com>
2023-11-20 22:21:09 -0800

    [fix] Fix Home View reload after modal close

--- word-app-ios/views/HomeView.swift
+++ word-app-ios/views/HomeView.swift
@@ -8,7 +8,6 @@ struct HomeView: View {
     private var show_createTable_modal = false
     
     var body: some View {
-        //NavigationView {
         ScrollView {
             VStack(alignment: .leading, spacing: 20) {
                 Text("Home")
@@ -16,26 +15,19 @@ struct HomeView: View {
                     .fontWeight(.black)
                 HStack(spacing: 120) {
                     Text("Your table")
-                    //NavigationLink(destination: CreateTableView()) {
-                    //    Text("Create Table")
-                    //}
-                    //.buttonStyle(ActionButton())
                     Button("Create Table") {
                         show_createTable_modal.toggle()
                     }
                     .fullScreenCover(isPresented: $show_createTable_modal, onDismiss: {
-                        // code to execute when sheet dismiss
-                        print(">>> this is being called")
-                        TableApi().getTables { (res) in
-                            self.get_tables_res = res
-                        }
-                      }, content: {
+                        sleep(1)
+                        reload_view()
+                    }, content: {
                         CreateTableView(showModal: self.$show_createTable_modal)
                     })
                 }
                 VStack(alignment: .leading, spacing: 5) {
                     if let tables = get_tables_res?.tables as? [Table] {
-                        ForEach(tables) { t in
+                        ForEach(tables, id: \.id) { t in
                             Button {
                                 print("hehehehe")
                             } label: {
@@ -45,23 +37,18 @@ struct HomeView: View {
                         }
                     }
                 }
-                .onChange(of: get_tables_res) { newValue in
-                    //TableApi().getTables { (res) in
-                    //    self.get_tables_res = res
-                    //}
-                    print(">>> value changed")
-                }
                 .onAppear() {
-                    TableApi().getTables { (res) in
-                        self.get_tables_res = res
-                    }
+                    reload_view()
                 }
             }
-            
         }
         .frame(maxWidth: 300)
-        //}
-        
+    }
+    
+    func reload_view() {
+        TableApi().getTables { (res) in
+            self.get_tables_res = res
+        }
     }
 }
 
